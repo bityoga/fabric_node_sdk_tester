adc56ccaca4cfef176e2665c6de564f6e93badc2
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-08-13 11:50:06 +0200

    - Updated for fabric v_2.2

--- get_tls_certificates.sh
+++ get_tls_certificates.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -x #echo on
 
-IP_ADDRESS="178.62.207.235"
-REMOTE_MACHINE_ORDERER_TLS_CERT_FILE="/root/hlft-store/orderer/tls-msp/tlscacerts/tls-tlsca-7054.pem"
-REMOTE_MACHINE_PEER2_TLS_CERT_FILE="/root/hlft-store/peer2/tls-msp/tlscacerts/tls-tlsca-7054.pem"
-LOCAL_ORDER_TLS_CERT_FILE="./hlft-store/orderer/tls-msp/tlscacerts/tls-tlsca-7054.pem"
-LOCAL_ORDER_PEER2_CERT_FILE="./hlft-store/peer2/tls-msp/tlscacerts/tls-tlsca-7054.pem"
+IP_ADDRESS="164.90.214.237"
+REMOTE_MACHINE_TLS_CERT_FILE="/root/hlft-store/hlfMSP/tlscacerts/tls-tlsca-7054.pem"
+LOCAL_TLS_CERT_FILE="./hlft-store/hlfMSP/tlscacerts/tls-tlsca-7054.pem"
 
-scp -r root@$IP_ADDRESS:$REMOTE_MACHINE_ORDERER_TLS_CERT_FILE $LOCAL_ORDER_TLS_CERT_FILE &&
-scp -r root@$IP_ADDRESS:$REMOTE_MACHINE_PEER2_TLS_CERT_FILE $LOCAL_ORDER_PEER2_CERT_FILE
+scp -r root@$IP_ADDRESS:$REMOTE_MACHINE_TLS_CERT_FILE $LOCAL_TLS_CERT_FILE
